7f5492c451ec54eb475384d170b45eab72057d9d
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2017-10-23 09:23:23 -0300

    License added and commentaries removed

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+/*
+
+Author: Luzenildo de Sousa Batista Junior
+E-Mail: luzejunior94@gmail.com
+
+License:
+
+MIT License
+
+Copyright (c) 2017 Luzenildo de Sousa Batista Junior
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -38,13 +66,8 @@ int main(int argc, char* argv[]){
   }
 
   SalesMan* s1 = new SalesMan(g1);
-
-  //s1->startedNodeIndex = 0;
-  //vector<int> vector;
-  //int j = 1;
-  //cout << "name: " << s1->graph->nodeVector[0]->name << " value: " << s1->graph->nodeVector[0]->vVector[j].value << " going to name: " << s1->graph->nodeVector[0]->vVector[j].nextNode->name << endl;
   s1->startAlgorithm(0);
-  cout << "The value is: " << s1->minorPath << endl;
+  cout << "The Minor value is: " << s1->minorPath << endl;
 
   return 0;
 }
